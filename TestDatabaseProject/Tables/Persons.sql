c083bff1a382d7fc8a5c13605464a8a5d31b36e9
Pavel <murzaevp@gmail.com>
2020-12-04 17:33:13 +0400

    #TDP-5 Добавление поля DateInput

--- TestDatabaseProject/Tables/Persons.sql
+++ TestDatabaseProject/Tables/Persons.sql
@@ -4,7 +4,8 @@
     [Surname] NVARCHAR(50) NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Patronimic] NVARCHAR(50) NULL, 
-    [DateOfBirth] DATE NULL
+    [DateOfBirth] DATE NULL, 
+    [DateInput] DATETIME NULL
 )
 
 GO
@@ -61,3 +62,12 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'Persons',
     @level2type = N'COLUMN',
     @level2name = 'DateOfBirth'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Дата добавления записи',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'DateInput'
